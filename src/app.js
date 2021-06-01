8f9869b11b594ff17f8660d82188fc371f7f5e96
Adriano Cappelletti <adriano.contacto@gmail.com>
2021-06-01 13:47:00 -0300

    Handles api info
    Write functions to get the images of the pokemons and their names
    and display them on the screen

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
-const amountOfPokemons = 20
+const amountOfPokemons = 20;
+let currentPage = 0;
+
+
+const fetchApi = (pokemon) => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}?limit=0&offset=${currentPage}`)
+    .then((response) => {
+        return response.json();
+    })
+    .then((responseJSON) => {
+        createPokemonFront(amountOfPokemons, responseJSON)
+    }).catch(error=>{
+        console.error(error)
+    });
+};
+
+const handlePokemons = () =>{
+    for (let i = currentPage; i < currentPage + 20; i++) {
+        fetchApi(i+1)
+    }
+}
+
+handlePokemons()
+
--- src/utility.js
+++ src/utility.js
@@ -1,15 +1,20 @@
 const $pokemonsContainer = document.querySelector('.pokemons-container')
 
 
-function createPokemonFront(amountOfPokemons){
+function createPokemonFront(amountOfPokemons, pokemonData){
+    let pokemonImg = pokemonData.sprites.front_default
+    let pokemonName = pokemonData.name
     const $pokemonContainer = document.createElement('div')
-    $pokemonContainer.className = 'pokemons-container__pokemon'
+    const $cardFront = document.createElement('div')
     let $pokemonImg = document.createElement('img')
     let $pokemonNameParagraph = document.createElement('p')
+    $pokemonContainer.className = 'pokemons-container__pokemon'
+    $cardFront.className = 'pokemon__front'
     $pokemonImg.src = pokemonImg
-    $npokemonNameParagraph = pokemonName
-    $pokemonContainer.appendChild($pokemonImg)
-    $pokemonContainer.appendChild($pokemonName)
+    $pokemonNameParagraph.textContent = pokemonName
+    $cardFront.appendChild($pokemonImg)
+    $cardFront.appendChild($pokemonNameParagraph)
+    $pokemonContainer.appendChild($cardFront)
     $pokemonsContainer.appendChild($pokemonContainer)    
 }
 
