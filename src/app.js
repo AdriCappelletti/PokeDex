5f469d7b6add26e71aee4ed3a11b4b17807870f3
Adriano Cappelletti <adriano.contacto@gmail.com>
2021-06-28 20:48:59 -0300

    Add Estlint

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
+const $btnPrev = document.querySelector('.btn-prev');
+const $btnNext = document.querySelector('.btn-next');
+const $mainContainer = document.querySelector('#pokemons');
+const $pagination = document.querySelector('.pagination');
 
-const $btnPrev = document.querySelector(".btn-prev");
-const $btnNext = document.querySelector(".btn-next");
-const $mainContainer = document.querySelector("#pokemons");
-const $pagination = document.querySelector('.pagination')
-
-let fetchedPokemons = []
-
+let fetchedPokemons = [];
 
 const amountOfPokemons = 20;
 let clickCounter = 0;
@@ -13,86 +11,75 @@ let interactionCounter = 0;
 let currentPage = 0;
 
 const fetchApi = () => {
-    fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${currentPage}`)
+  fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${currentPage}`)
 
-    .then((response) => {
-        return response.json();
-    })
+    .then((response) => response.json())
     .then((responseJSON) => {
+      handlePokemonData(responseJSON);
 
-        handlePokemonData(responseJSON);
-
-        fetchedPokemons = []
-
+      fetchedPokemons = [];
     })
     .catch((error) => {
-        console.error(error);
+      console.error(error);
     });
 };
 fetchApi();
 
-
-const getPokemonInfo = (pokemonName,$card) => {
-  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then((response) => {
-    return response.json();
-  }).then((responseJSON)=>{
-      setCardBackInfo(responseJSON, $card)
-  }).catch((error) =>{
-      console.error(error)
+const getPokemonInfo = (pokemonName, $card) => {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then((response) => response.json()).then((responseJSON) => {
+    setCardBackInfo(responseJSON, $card);
+  }).catch((error) => {
+    console.error(error);
   });
 };
 
 const handlePokemonData = (pokemonData) => {
-  for (let i = 0; i < pokemonData["results"].length; i++) {
+  for (let i = 0; i < pokemonData.results.length; i++) {
     const keys = Object.keys(pokemonData.results);
-    let index = keys[i];
+    const index = keys[i];
     createPokemonCard(index, pokemonData);
   }
 };
 
-
-$btnNext.addEventListener("click", () => {
+$btnNext.addEventListener('click', () => {
   interactionCounter += 1;
-  clickCounter = clickCounter + 1;
-  currentPage = currentPage + 20;
+  clickCounter += 1;
+  currentPage += 20;
   fetchApi();
 });
 
-$btnPrev.addEventListener("click", () => {
+$btnPrev.addEventListener('click', () => {
   if (clickCounter > 0) {
-    clickCounter = clickCounter - 1;
-    currentPage = currentPage - 20;
+    clickCounter -= 1;
+    currentPage -= 20;
     fetchApi();
   } else if (clickCounter < 1) {
-    return "";
+    return '';
   }
 });
 
-$mainContainer.addEventListener('click', (e)=>{ const $card = e.target.parentNode
-    const pokemonName = $card.id
-    if (fetchedPokemons.includes(pokemonName)) {
-      $card.classList.toggle('flipped')
-    }
-    else if ($card.classList.contains('pokemons-container__pokemon')) {
-      fetchedPokemons.push(pokemonName)
+$mainContainer.addEventListener('click', (e) => {
+  const $card = e.target.parentNode;
+  const pokemonName = $card.id;
+  if (fetchedPokemons.includes(pokemonName)) {
+    $card.classList.toggle('flipped');
+  } else if ($card.classList.contains('pokemons-container__pokemon')) {
+    fetchedPokemons.push(pokemonName);
 
-      getPokemonInfo(pokemonName, $card)
-      $card.classList.toggle('flipped')
-    }})
-
-
-$pagination.addEventListener('click', (e)=>{
-  const $btnPrev = document.querySelector('.btn-prev')
-  const $btnNext = document.querySelector('.btn-next')
-  const selectedBtn = e.target
-  if (selectedBtn.classList.contains('btn-next')) {
-    selectedBtn.classList.add('focused')
-    $btnPrev.classList.remove('focused')
-  } else if (selectedBtn.classList.contains('btn-prev')) {
-    selectedBtn.classList.add('focused')
-    $btnNext.classList.remove('focused')
+    getPokemonInfo(pokemonName, $card);
+    $card.classList.toggle('flipped');
   }
-  
+});
 
-})
-
+$pagination.addEventListener('click', (e) => {
+  const $btnPrev = document.querySelector('.btn-prev');
+  const $btnNext = document.querySelector('.btn-next');
+  const selectedBtn = e.target;
+  if (selectedBtn.classList.contains('btn-next')) {
+    selectedBtn.classList.add('focused');
+    $btnPrev.classList.remove('focused');
+  } else if (selectedBtn.classList.contains('btn-prev')) {
+    selectedBtn.classList.add('focused');
+    $btnNext.classList.remove('focused');
+  }
+});
