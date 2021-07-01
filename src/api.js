7f3afd2f866e450c32ed1e1234158aa6be109904
Adriano Cappelletti <adriano.contacto@gmail.com>
2021-06-30 22:39:54 -0300

    Separates the src in modules

---
+++ src/api.js
@@ -0,0 +1,14 @@
+export const fetchApi = (currentPage) => fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${currentPage}`)
+  .then((response) => response.json())
+  .then((responseJSON) => responseJSON)
+  .catch((error) => {
+    console.error(error);
+  });
+
+export const getPokemonInfo = (pokemonName) => {
+  try {
+    return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then((response) => response.json()).then((responseJSON) => responseJSON);
+  } catch (error) {
+    console.error(error);
+  }
+};
