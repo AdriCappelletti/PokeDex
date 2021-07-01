b87b4ae83107ea9ea87b3cf3b3a941cb1db4db73
Adriano Cappelletti <adriano.contacto@gmail.com>
2021-07-01 14:15:31 -0300

    Separate the code in modules

--- src/api.js
+++ src/api.js
@@ -1,9 +1,12 @@
-export const fetchApi = (currentPage) => fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${currentPage}`)
-  .then((response) => response.json())
-  .then((responseJSON) => responseJSON)
-  .catch((error) => {
+export const fetchApi = (currentPage) => {
+  try {
+    return fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${currentPage}`)
+      .then((response) => response.json())
+      .then((responseJSON) => responseJSON);
+  } catch (error) {
     console.error(error);
-  });
+  }
+};
 
 export const getPokemonInfo = (pokemonName) => {
   try {
