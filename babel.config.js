e78a8c1466542ca17d447a242b5e2005eb5eb0be
Adriano Cappelletti <adriano.contacto@gmail.com>
2021-08-01 20:32:00 -0300

    Add jest

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env',
+      { targets: { node: 'current' } },
+    ],
+  ],
+};
+
+// Jest corre pasa archivos por babel y luego genera archivos adecuados para node
+// y luego corre los tests sobre el js optimizado
